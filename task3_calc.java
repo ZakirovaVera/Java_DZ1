6d4a9efb50612fdd06ba2964f6e5e6191b572d67
ZakirovaVera <zakirovavk@gmail.com>
2022-11-24 00:23:11 +0500

    решение задачи калькулятор

--- task3_calc.java
+++ task3_calc.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 // 3. Реализовать простой калькулятор (пользователь вводит 2 числа и вводит операцию (+ - / *)
 
 public class task3_calc {
-    
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        int number1 = iScanner.nextInt();
+        System.out.print("Введите второе число: ");
+        int number2 = iScanner.nextInt();
+        System.out.print("Введите тип операции(+ - * /): ");
+        String operationCalc = iScanner.next();
+        float resultCalc = Calc(number1, number2, operationCalc);
+        // System.out.printf("%d ", number1);
+        // System.out.printf("%s ", operationCalc);
+        // System.out.printf("%d ",  number2);
+        // System.out.printf("%d",  resultCalc);
+        System.out.printf("%d %s %d = %f", number1, operationCalc, number2, resultCalc);
+
+
+    }
+    public static float Calc(int num1, int num2, String oper) {
+        float res = 0;
+        switch(oper){
+             
+            case "+": 
+                res = num1 + num2;
+                break;
+            case "-": 
+                res = num1 - num2;
+                break;
+            case "*": 
+                res = num1 * num2;
+                break;
+            case "/": 
+                res = num1 / num2;
+                break;
+            default:
+                System.out.println("Error");
+        }
+        return res;
+    }
 }
